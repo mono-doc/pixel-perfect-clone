87a836e7b3384c49bd8f2ec1fe0476cf63bc853f
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2025-12-23 22:17:12 +0200

    Update ApiSidebar.tsx

--- src/components/api/ApiSidebar.tsx
+++ src/components/api/ApiSidebar.tsx
@@ -30,7 +30,7 @@ const SectionLabel = ({ children }: { children: React.ReactNode }) => (
 
 const ApiSidebar = () => {
   return (
-    <aside className="w-64 flex-shrink-0 border-r border-border h-[calc(100vh-7rem)] sticky top-28 overflow-y-auto scrollbar-thin py-6 pr-4">
+    <aside className="w-64 flex-shrink-0 border-r border-border h-[calc(100vh-7rem)] sticky overflow-y-auto scrollbar-thin py-6 pr-4">
       {/* USING THE API */}
       <div className="mb-6">
         <SectionLabel>Using the API</SectionLabel>
